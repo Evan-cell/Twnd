f560bddba9087c6d56f371c677be2a6b53233ffe
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2023-08-08 12:20:40 +0300

    features

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Nav from './components/Nav'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Features from './components/Features'
 
 function App() {
   
@@ -11,6 +12,7 @@ function App() {
     <div className=''>
      <Navbar />
      <Hero />
+     <Features />
     </div>
   )
 }
